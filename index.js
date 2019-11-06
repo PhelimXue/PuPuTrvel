e8f228917a61dd1721bc7da5c8290a57e67d5e50
中冠-薛普嘉 <I27962@icsc.com.tw>
2019-11-06 11:28:26 +0800

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const menuList = require('./manu.js');
 
 // create LINE SDK config from env variables
 const config = {
@@ -28,215 +29,6 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-var mySchedule = {
-  "type": "template",
-  "altText": "無法顯示選單",
-  "template": {
-    "type": "carousel",
-    "columns": [
-      {
-        "text": "4/25 - 4/27",
-        "actions": [
-          {
-            "type": "message",
-            "label": "4/25 (週六)",
-            "text": "1日行程"
-          },
-          {
-            "type": "message",
-            "label": "4/26 (週日)",
-            "text": "2日行程"
-          },
-          {
-            "type": "message",
-            "label": "4/27 (週一)",
-            "text": "3日行程"
-          }
-        ]
-      },
-      {
-        "text": "4/28 - 4/30",
-        "actions":[
-          {
-            "type": "message",
-            "label": "4/28 (週二)",
-            "text": "4日行程"
-          },
-          {
-            "type": "message",
-            "label": "4/29 (週三)",
-            "text": "5日行程"
-          },
-          {
-            "type": "message",
-            "label": "4/30 (週四)",
-            "text": "6日行程"
-          }
-        ]
-      },
-      {
-        "text": "5/1 - 5/2",
-        "actions":[
-          {
-            "type": "message",
-            "label": "5/1 (週五)",
-            "text": "7日行程"
-          },
-          {
-            "type": "message",
-            "label": "5/2 (週六)",
-            "text": "8日行程"
-          },
-          {
-            "type": "message",
-            "label": "none",
-            "text": "none"
-          }
-        ]
-      }
-    ]
-  }
-};
-var wishList = {
-  "type": "template",
-  "altText": "無法顯示選單",
-  "template": {
-    "type": "carousel",
-    "columns": [
-      {
-        "text": "就在附近",
-        "actions": [
-          {
-            "type": "message",
-            "label": "上野",
-            "text": "清單-上野"
-          },
-          {
-            "type": "message",
-            "label": "秋葉原",
-            "text": "清單-秋葉原"
-          },
-          {
-            "type": "message",
-            "label": "淺草",
-            "text": "清單-淺草"
-          }
-        ]
-      },
-      {
-        "text": "想去就去",
-        "actions": [
-          {
-            "type": "message",
-            "label": "吉祥寺",
-            "text": "清單-吉祥寺"
-          },
-          {
-            "type": "message",
-            "label": "迪士尼",
-            "text": "清單-迪士尼"
-          },
-          {
-            "type": "message",
-            "label": "池袋",
-            "text": "清單-池袋"
-          }
-        ]
-      },
-      {
-        "text": "說走就走",
-        "actions": [
-          {
-            "type": "message",
-            "label": "涉谷",
-            "text": "清單-涉谷"
-          },
-          {
-            "type": "message",
-            "label": "原宿",
-            "text": "清單-原宿"
-          },
-          {
-            "type": "message",
-            "label": "新宿",
-            "text": "清單-新宿"
-          }
-        ]
-      },
-      {
-        "text": "難得走走",
-        "actions": [
-          {
-            "type": "message",
-            "label": "台場",
-            "text": "清單-台場"
-          },
-          {
-            "type": "message",
-            "label": "河口湖",
-            "text": "清單-河口湖"
-          },
-          {
-            "type": "message",
-            "label": "輕井澤",
-            "text": "清單-輕井澤"
-          }
-        ]
-      }
-    ]
-  }
-};
-
-var odaibaList = {
-  "type": "template",
-  "altText": "無法顯示選單",
-  "template": {
-    "type": "buttons",
-    "text": "台場景點",
-    "actions": [
-      {
-        "type": "message",
-        "label": "AQUA CITY",
-        "text": "普普-AQUA CITY"
-      },
-      {
-        "type": "message",
-        "label": "台場購物廣場",
-        "text": "普普-台場購物廣場"
-      },
-      {
-        "type": "message",
-        "label": "大江戶溫泉物語",
-        "text": "普普-大江戶溫泉物語"
-      }
-    ]
-  }
-};
-var karuizawaList = {
-  "type": "template",
-  "altText": "無法顯示選單",
-  "template": {
-    "type": "buttons",
-    "text": "輕井澤景點",
-    "actions": [
-      {
-        "type": "message",
-        "label": "舊輕井澤銀座",
-        "text": "普普-舊銀座"
-      },
-      {
-        "type": "message",
-        "label": "雲場池",
-        "text": "普普-雲場池"
-      },
-      {
-        "type": "message",
-        "label": "輕井澤Outlet",
-        "text": "普普-輕井澤Outlet"
-      }
-    ]
-  }
-};
 /*
 */
 var itinerary = 'https://docs.google.com/document/d/1Whq9JfMWs7t5Oq3DtTQQY5vnlDAvGjw6zrnod1RBuLE/edit?usp=sharing';
@@ -248,7 +40,7 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
   var input = event.message.text;
-  var reply = '有什麼問題問我，請輸入關鍵字：\n行程表、想去、想吃、成員';
+  var reply = '有什麼問題問我，請輸入關鍵字：\n行程表、行程、想去、想吃、成員';
 
   // 第一段貼行程
   if(input.includes('行程')){
@@ -308,7 +100,7 @@ function handleEvent(event) {
           return client.replyMessage(event.replyToken, karuizawaList);
           break;
       case '清單-河口湖':
-          return client.replyMessage(event.replyToken, karuizawaList);
+          return client.replyMessage(event.replyToken, kawaguchikoList);
           break;
       case '清單-上野':
           return client.replyMessage(event.replyToken, karuizawaList);
